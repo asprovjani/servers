0e9f1c8144ba95ca810fcf6f4bb876eb7ef293cb
asprovjani <asprov.jani@gmail.com>
2021-12-27 00:23:07 +0100

    Backend works

---
+++ springDemo/src/main/java/com/example/springDemo/SpringDemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.springDemo;
+
+import com.example.springDemo.model.Server;
+import com.example.springDemo.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.example.springDemo.enumeration.Status.SERVER_DOWN;
+import static com.example.springDemo.enumeration.Status.SERVER_UP;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.2.170", "Windows", "16 GB", "Personal PC", "http://localhost:8080/server/image/server4.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
+		};
+	}
+}
---
+++ springDemo/src/main/java/com/example/springDemo/model/Server.java
@@ -0,0 +1,33 @@
+package com.example.springDemo.model;
+
+import com.example.springDemo.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address can not be empty or null")
+    private String ipAddress;
+
+    private String name;
+    private String memory;
+    private String type;
+    private String imgUrl;
+    private Status status;
+}
